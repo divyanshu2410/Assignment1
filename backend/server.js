a9fa08eb721cb40bdc32d2e18556c166ccdb6157
divyanshu2410 <141527068+divyanshu2410@users.noreply.github.com>
2024-04-28 16:50:04 +0000

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const adminRoutes = require("./routes/adminRoutes");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
