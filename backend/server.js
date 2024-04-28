79ac6693739914d7ba345f8eef206d1f87d5ad93
divyanshu2410 <141527068+divyanshu2410@users.noreply.github.com>
2024-04-28 16:58:32 +0000

    Intial commit

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,20 @@ const adminRoutes = require("./routes/adminRoutes");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5001;
+// const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(
+    "mongodb+srv://divyanshukumar736:hvlvLJsxoNbqZVIV@cluster0.ln2mfmi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+    app.listen(5000);
+    // console.log(`Server is running on port ${PORT}`);
+    // });
     console.log("Connected to MongoDB");
   })
   .catch((error) => {
