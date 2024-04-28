3ac3dc8408ff99c7d63ac137fcd67c195bc70aaa
divyanshu2410 <141527068+divyanshu2410@users.noreply.github.com>
2024-04-28 16:48:08 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -9,20 +9,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
     console.log("Connected to MongoDB");
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error.message);
   });
-
-app.use("/api/auth", authRoutes);
-app.use("/api/admin", adminRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
