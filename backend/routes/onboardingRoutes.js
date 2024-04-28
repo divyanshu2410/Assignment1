c5058afa793435cdbc7c10b7e3640b29e0a920e1
divyanshu2410 <141527068+divyanshu2410@users.noreply.github.com>
2024-04-28 21:49:52 +0000

    Intial commit

--- backend/routes/onboardingRoutes.js
+++ backend/routes/onboardingRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Onboarding = require("../models/onboarding");
+const Onboarding = require("../models/Onboarding");
 
 router.post("/", async (req, res) => {
   try {
